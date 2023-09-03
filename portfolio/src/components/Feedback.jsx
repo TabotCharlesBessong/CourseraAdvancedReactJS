0698bc8886166bd5f0267298b9f7aaf1362aed7e
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-03 02:12:32 +0100

    completing basic feedback

--- portfolio/src/components/Feedback.jsx
+++ portfolio/src/components/Feedback.jsx
@@ -3,11 +3,17 @@ import React from 'react'
 import { useState } from 'react'
 
 const Feedback = () => {
-  const [score, setScore] = useState(7)
+  const [score, setScore] = useState('7')
   const [comment, setComment] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    if(Number(score) <= 5 && comment.length <= 10){
+      alert('Please provide a coomment why the experience was poor')
+    }
+    console.log({score,comment})
+    setComment('')
+    setScore('10')
   }
   return (
     <form
@@ -25,7 +31,7 @@ const Feedback = () => {
         <div className="Field" style={{ margin: "auto", width: "400px" }}>
           <label htmlFor="">Score</label>
           <input
-            onChange={(e) => setComment(e.target.value)}
+            onChange={(e) => setScore(e.target.value)}
             style={{ width: "90%" }}
             type="range"
             name=""
